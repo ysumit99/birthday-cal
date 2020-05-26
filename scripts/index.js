3457fef8b0e401ef504123fda241948d2119add2
Sumit Yadav <ysumit99@gmail.com>
2020-05-26 21:21:38 +0530

    get random color function

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,25 @@
+//input data
 let birthdays = [];
 let year;
 
 //birthdays by day of week
 let segregatedBirthdays = {
-    "monday" : [],
-    "tuesday" : [],
-    "wednesday" : [],
-    "thursday" : [],
-    "friday" : [],
-    "saturday" : [],
-    "sunday" : []
+    "monday": [],
+    "tuesday": [],
+    "wednesday": [],
+    "thursday": [],
+    "friday": [],
+    "saturday": [],
+    "sunday": []
 };
 
 
 
 
 let getDayOfWeek = (date) => {
-    const dayOfWeek = new Date(date).getDay();    
-    return isNaN(dayOfWeek) ? null : 
-      ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'][dayOfWeek];
+    const dayOfWeek = new Date(date).getDay();
+    return isNaN(dayOfWeek) ? null :
+        ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'][dayOfWeek];
 }
 
 
@@ -27,11 +28,11 @@ let compareDates = (a, b) => {
     var aa = a.birthday.split('/').reverse().join(),
         bb = b.birthday.split('/').reverse().join();
 
-    return aa  > bb ? -1 : (aa < bb ? 1 : 0);
+    return aa > bb ? -1 : (aa < bb ? 1 : 0);
 }
 
 let getDimension = (id) => {
-   
+
 }
 
 let getInitials = (name) => {
@@ -39,16 +40,22 @@ let getInitials = (name) => {
     return initials[0][0] + initials[1][0];
 }
 
+const getRandomColor = () => {
+
+    const colorsArray = ["#6EE33B", "#F6982D", "#FA4861", "#F959ED", "#53B8F6", "#728FDC", "#373AEF", "#F776AA", "#c7ff20", "#ff81fa", "#ff956a"];
+    return colorsArray[Math.floor((Math.random() * colorsArray.length))];
+}
+
 let createSquare = (person) => {
 
-    
+
     let newElement = document.createElement("div");
     newElement.innerHTML = getInitials(person.name);
     newElement.style = `
         display: flex;
         justify-content: center;
         align-items: center;
-        background: grey;
+        background: ${getRandomColor()};
         border: 1px solid red;
         color: white;
         font-family: 'Open Sans', sans-serif;
@@ -58,48 +65,48 @@ let createSquare = (person) => {
 }
 
 let getCardSelector = (day) => {
-    
+
     let selector = '';
 
-    switch(day){
+    switch (day) {
 
-        case 'monday' : 
+        case 'monday':
             selector = 'card-mon';
             break;
 
-        case 'tuesday' : 
+        case 'tuesday':
             selector = 'card-tue';
             break;
 
-        case 'wednesday' : 
+        case 'wednesday':
             selector = 'card-wed';
             break;
 
-        case 'thursday' : 
+        case 'thursday':
             selector = 'card-thur';
             break;
 
-        case 'friday' : 
+        case 'friday':
             selector = 'card-fri';
             break;
 
-        case 'saturday' : 
+        case 'saturday':
             selector = 'card-sat';
             break;
 
-        case 'sunday' : 
+        case 'sunday':
             selector = 'card-sun';
             break;
 
-        
+
     }
 
     return selector;
 }
 
 let fillCard = () => {
-    
-    for(day in segregatedBirthdays ){
+
+    for (day in segregatedBirthdays) {
 
         //get card dimensions
         let id = getCardSelector(day);
@@ -109,24 +116,25 @@ let fillCard = () => {
         let personCount = segregatedBirthdays[day].length;
 
         //square count
-        let squaresPerRow = Math.floor(Math.sqrt(personCount))+1;
+        let squaresPerRow = Math.floor(Math.sqrt(personCount)) + 1;
 
         //single square dimension
-        let squareDimension = cardWidth/squaresPerRow;
+        let squareDimension = cardWidth / squaresPerRow;
 
         //dynamically create grid 
         document.getElementById(id).style.gridTemplateColumns = `repeat(${squaresPerRow}, ${squareDimension}px)`;
         document.getElementById(id).style.gridTemplateRows = `repeat(${squaresPerRow}, ${squareDimension}px)`;
-        
+
         //create squares
         segregatedBirthdays[day].forEach(person => {
 
-           let newSquare =  createSquare(person);
-           document.getElementById(id).appendChild(newSquare);
+            let newSquare = createSquare(person);
+            document.getElementById(id).appendChild(newSquare);
+
 
         })
 
-       
+
 
     }
 
@@ -135,31 +143,31 @@ let fillCard = () => {
 let getData = (e) => {
 
     e.preventDefault();
-   
-   //get input data
-   birthdays = document.getElementById('json-input').value;
-   year = document.getElementById('year-input').value;
-   
-  //parse data
-   birthdays = JSON.parse(birthdays);
- 
- //sort birthday by youngest to oldest
- let sortedBirthdays = birthdays.sort(compareDates);
- console.log(sortedBirthdays);
 
- 
- sortedBirthdays.forEach((person) => {
-   
-  //segregate birthdays by day of the week
-   segregatedBirthdays[getDayOfWeek(person.birthday)].push(person);
+    //get input data
+    birthdays = document.getElementById('json-input').value;
+    year = document.getElementById('year-input').value;
 
- });
+    //parse data
+    birthdays = JSON.parse(birthdays);
 
- console.log(segregatedBirthdays);
+    //sort birthday by youngest to oldest
+    let sortedBirthdays = birthdays.sort(compareDates);
+    console.log(sortedBirthdays);
 
-  fillCard();
-  
-  
+
+    sortedBirthdays.forEach((person) => {
+
+        //segregate birthdays by day of the week
+        segregatedBirthdays[getDayOfWeek(person.birthday)].push(person);
+
+    });
+
+
+    // fill card
+    fillCard();
+
+
 
 }
 
